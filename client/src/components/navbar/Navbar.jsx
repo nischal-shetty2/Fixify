8874535174c1a48c9da3165a7aef538ee8d63773
Nischal Shetty <nischal2004coc@gmail.com>
2024-12-10 15:55:26 +0530

    upgraded vite version

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -30,14 +30,12 @@ const Navbar = () => {
     (async () => {
       setIsLoading(true);
       try {
-        const { data } = await axiosFetch.get('/auth/me');
+        const { data } = await axiosFetch.get("/api/auth/me");
         setUser(data.user);
-      }
-      catch({ response }) {
-        localStorage.removeItem('user');
+      } catch ({ response }) {
+        localStorage.removeItem("user");
         console.log(response.data.message);
-      }
-      finally {
+      } finally {
         setIsLoading(false);
       }
     })();
@@ -57,18 +55,18 @@ const Navbar = () => {
   const menuLinks = [
     { path: "/gigs?category=design", name: "Graphics & Design" },
     { path: "/gigs?category=video", name: "Video & Animation" },
-    { path: "/gigs?category=writing&Translation", name: "Writing & Translation" },
+    {
+      path: "/gigs?category=writing&Translation",
+      name: "Writing & Translation",
+    },
     { path: "/gigs?category=ai", name: "AI Services" },
     { path: "/gigs?category=social", name: "Digital Marketing" },
     { path: "/gigs?category=voice", name: "Music & Audio" },
     { path: "/gigs?category=tech", name: "Programming & Tech" },
-    {path: "/gigs?category=illustration", name: "Illustration" },
-    {path: "/gigs?category=seo", name: "SEO"},
-    {path: "/gigs?category=books", name: "Books"},
-    {path: "/gigs?category=dataentry", name: "Data Entry"}
-
-
-
+    { path: "/gigs?category=illustration", name: "Illustration" },
+    { path: "/gigs?category=seo", name: "SEO" },
+    { path: "/gigs?category=books", name: "Books" },
+    { path: "/gigs?category=dataentry", name: "Data Entry" },
   ];
 
   const settings = {
@@ -102,8 +100,8 @@ const Navbar = () => {
 
   const handleLogout = async () => {
     try {
-      await axiosFetch.post("/auth/logout");
-      localStorage.removeItem('user');
+      await axiosFetch.post("/api/auth/logout");
+      localStorage.removeItem("user");
       setUser(null);
       navigate("/");
     } catch ({ response }) {
@@ -123,13 +121,19 @@ const Navbar = () => {
 
         <div className="links">
           <div className="menu-links">
-          <Link to="/#workhiveCommunity" className="link" onClick={() => handleScrollTo("workhiveCommunity")}>
+            <Link
+              to="/#workhiveCommunity"
+              className="link"
+              onClick={() => handleScrollTo("workhiveCommunity")}>
               Workhive Community
             </Link>
             <span>
-            <Link to="/#explores" className="link" onClick={() => handleScrollTo("explores")}>
-              Explore
-            </Link>
+              <Link
+                to="/#explores"
+                className="link"
+                onClick={() => handleScrollTo("explores")}>
+                Explore
+              </Link>
             </span>
             <span>English</span>
             {!user?.isSeller && <span>Become a Seller</span>}
@@ -146,10 +150,12 @@ const Navbar = () => {
                 </span>
               )}
               {!user && (
-                <Link to="/register" className={`link ${showMenu || pathname !== "/" ? "join-active" : ""}`}>
-                    <button>
-                      Join
-                    </button>
+                <Link
+                  to="/register"
+                  className={`link ${
+                    showMenu || pathname !== "/" ? "join-active" : ""
+                  }`}>
+                  <button>Join</button>
                 </Link>
               )}
               {user && (
